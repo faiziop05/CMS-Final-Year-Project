b5f28ddd180202c409430bc6d2b23fafeac07738
Faizan Hanif <faizanst123@gmail.com>
2024-10-03 19:50:55 +0500

    Ui Pics Added

--- Student-Frontend-(React-Native)/Console/IpAddress.js
+++ Student-Frontend-(React-Native)/Console/IpAddress.js
@@ -1,10 +1,10 @@
 //ipAddress.js file
-// const address="192.168.18.25";
+const address="192.168.18.25";
 //  const address="172.20.10.7";
 //  const address="172.20.10.7";
 //  const address="10.44.74.103";
 //  const address="10.44.108.195";
 //  const address="10.44.107.151";
- const address="10.44.106.161";
+//  const address="10.44.106.161";
  
  export default address
